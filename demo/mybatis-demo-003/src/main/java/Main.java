a5755fd15b5894801360820be8b17871d383f400
letian <sunlt1699@gmail.com>
2018-11-03 15:46:59 +0800

    不用配置

--- demo/mybatis-demo-003/src/main/java/Main.java
+++ demo/mybatis-demo-003/src/main/java/Main.java
@@ -1,14 +1,10 @@
 import java.beans.PropertyVetoException;
-import java.io.IOException;
 
 import datasource.C3P0DataSource;
 import datasource.DBCPDataSource;
 import datasource.MyBatisDataSource;
 import datasource.TomcatDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.dbcp2.BasicDataSource;
-import org.apache.commons.pool2.impl.GenericObjectPool;
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
@@ -27,31 +23,11 @@ import javax.sql.DataSource;
 @Slf4j
 public class Main {
 
-    @Test
-    public void test_01() throws IOException {
-        SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-        SqlSessionFactory sessionFactory;
-
-        sessionFactory = sqlSessionFactoryBuilder.build(
-                Resources.getResourceAsReader("mybatis-config.xml"),
-                "development"  // 这个参数可以省略，因为 mybatis-config.xml 的<environments>标签指定了默认环境为development
-        );
-
-        SqlSession sqlSession = sessionFactory.openSession();
-
-        // 以上是样板代码
-        // 以下是「业务逻辑」
-
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = userMapper.findById(1L);
-        log.info("{}", user);
-    }
-
     /**
      * 测试 mybatis 自带连接池
      */
     @Test
-    public void test_02() {
+    public void test_01() {
         DataSource dataSource = MyBatisDataSource.get();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
@@ -60,10 +36,13 @@ public class Main {
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 
         SqlSession sqlSession = sessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-
-        User user = userMapper.findById(1L);
-        log.info("{}", user);
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findById(1L);
+            log.info("{}", user);
+        } finally {
+            sqlSession.close();
+        }
     }
 
 
@@ -71,7 +50,7 @@ public class Main {
      * 测试 dbcp 连接池
      */
     @Test
-    public void test_03() {
+    public void test_02() {
         DataSource dataSource = DBCPDataSource.get();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
@@ -80,17 +59,20 @@ public class Main {
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 
         SqlSession sqlSession = sessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-
-        User user = userMapper.findById(1L);
-        log.info("{}", user);
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findById(1L);
+            log.info("{}", user);
+        } finally {
+            sqlSession.close();
+        }
     }
 
     /**
      * 测试 c3p0 连接池
      */
     @Test
-    public void test_04() throws PropertyVetoException {
+    public void test_03() throws PropertyVetoException {
         DataSource dataSource = C3P0DataSource.get();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
@@ -99,17 +81,20 @@ public class Main {
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 
         SqlSession sqlSession = sessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-
-        User user = userMapper.findById(1L);
-        log.info("{}", user);
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findById(1L);
+            log.info("{}", user);
+        } finally {
+            sqlSession.close();
+        }
     }
 
     /**
      * 测试 tomcat jdbc 连接池
      */
     @Test
-    public void test_05() {
+    public void test_04() {
         DataSource dataSource = TomcatDataSource.get();
         TransactionFactory transactionFactory = new JdbcTransactionFactory();
         Environment environment = new Environment("development", transactionFactory, dataSource);
@@ -118,10 +103,13 @@ public class Main {
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(configuration);
 
         SqlSession sqlSession = sessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-
-        User user = userMapper.findById(1L);
-        log.info("{}", user);
+        try {
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user = userMapper.findById(1L);
+            log.info("{}", user);
+        } finally {
+            sqlSession.close();
+        }
     }
 
 }
